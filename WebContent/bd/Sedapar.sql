325a9dd61d38462c3a2ec9d8251ea71893cfef73
ilysander <tfapaza@cibertec.edu.pe>
2013-12-24 17:37:29 -0500

    Modificacion en la BD

--- WebContent/bd/Sedapar.sql
+++ WebContent/bd/Sedapar.sql
@@ -2,12 +2,21 @@ create database DB_Sedapar;
 use DB_Sedapar;
 
 create table tb_perfil(
-idPerfil int primary key,
-desPerfil varchar(50)
+idPerfil int primary key auto_increment,
+desPerfil varchar(50),
+moduloContratos tinyint,
+moduloCategorias tinyint,
+moduloLiquidacion tinyint,
+moduloReportes tinyint,
+moduloManClientes tinyint,
+moduloManPerfiles tinyint,
+moduloManUsuarios tinyint,
+moduloBuzon tinyint,
+moduloCalendario tinyint
 );
 
 create table tb_usuario(
-idUsuario int primary key,
+idUsuario int primary key auto_increment,
 idPerfil int references tb_perfil(idPerfil),
 userUsuario varchar(10),
 passUsuario varchar(10),
@@ -18,13 +27,16 @@ dniUsuario varchar(10)
 );
 
 /*Datos en Perfiles*/
-INSERT INTO tb_perfil VALUES(1,'Administrador');
-INSERT INTO tb_perfil VALUES(2,'Tecnico');
-INSERT INTO tb_perfil VALUES(3,'Cajero');
+INSERT INTO tb_perfil VALUES(null,'Administrador',1,1,1,1,1,1,1,1,1);
+INSERT INTO tb_perfil VALUES(null,'Tecnico',1,1,1,1,1,1,1,1,1);
+INSERT INTO tb_perfil VALUES(null,'Cajero',1,1,1,1,1,1,1,1,1);
 
 /*Datos en Usuarios*/
-INSERT INTO tb_usuario VALUES(1,1,'lysander','root','Felix','Apaza','Arroyo','43836055');
-INSERT INTO tb_usuario VALUES(2,1,'rdelgado','testing','Renzo','Delgado','Guerra','89999992');
-INSERT INTO tb_usuario VALUES(3,2,'oldkefka','qv2','Ricardo','Quevedo','Grimaldo','89999991');
-INSERT INTO tb_usuario VALUES(4,3,'csonan','testing','Carlos','Sonan','Yonashiro','89999993');
-INSERT INTO tb_usuario VALUES(5,3,'acoquis','testing','Andre','Coquis','Raffo','89999994');
+INSERT INTO tb_usuario VALUES(null,1,'lysander','root','Felix','Apaza','Arroyo','43836055');
+INSERT INTO tb_usuario VALUES(null,1,'rdelgado','testing','Renzo','Delgado','Guerra','89999992');
+INSERT INTO tb_usuario VALUES(null,2,'oldkefka','qv2','Ricardo','Quevedo','Grimaldo','89999991');
+INSERT INTO tb_usuario VALUES(null,3,'csonan','testing','Carlos','Sonan','Yonashiro','89999993');
+INSERT INTO tb_usuario VALUES(null,3,'acoquis','testing','Andre','Coquis','Raffo','89999994');
+
+
+
