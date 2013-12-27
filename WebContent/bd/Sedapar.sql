bde9bfccd2083d0481c061500af87b551336fa3a
Renzo425 <renzodelgadoguerra@gmail.com>
2013-12-27 07:50:41 -0500

    falto actualizar la bd, sorry

--- WebContent/bd/Sedapar.sql
+++ WebContent/bd/Sedapar.sql
@@ -26,6 +26,13 @@ apemaUsuario varchar(30),
 dniUsuario varchar(10)
 );
 
+create table tb_test_solicitud(
+num_solicitud int primary key auto_increment,
+nombre varchar(30),
+fecha_solicitud varchar(20),
+estado varchar(40) check(estado in ('Pendiente','Aprobada'))
+);
+
 /*Datos en Perfiles*/
 INSERT INTO tb_perfil VALUES(null,'Administrador',1,1,1,1,1,1,1,1,1);
 INSERT INTO tb_perfil VALUES(null,'Tecnico',1,1,1,1,1,1,1,1,1);
@@ -39,4 +46,12 @@ INSERT INTO tb_usuario VALUES(null,3,'csonan','testing','Carlos','Sonan','Yonash
 INSERT INTO tb_usuario VALUES(null,3,'acoquis','testing','Andre','Coquis','Raffo','89999994');
 
 
-
+/*Datos en TEST SOLICITUD*/
+INSERT INTO tb_test_solicitud VALUES(null,'Renzo Delgado','26-12-2013','Pendiente');
+INSERT INTO tb_test_solicitud VALUES(null,'Felix Apaza','26-12-2013','Pendiente');
+INSERT INTO tb_test_solicitud VALUES(null,'Ricardo Quevedo','26-12-2013','Pendiente');
+INSERT INTO tb_test_solicitud VALUES(null,'Carlos Sonan','26-12-2013','Pendiente');
+INSERT INTO tb_test_solicitud VALUES(null,'Andre Coquis','26-12-2013','Pendiente');
+INSERT INTO tb_test_solicitud VALUES(null,'Juan Carlos Espinoza','26-12-2013','Aprobada');
+INSERT INTO tb_test_solicitud VALUES(null,'Tilsa Lozano','26-12-2013','Pendiente');
+INSERT INTO tb_test_solicitud VALUES(null,'Loco Vargas','26-12-2013','Pendiente');
